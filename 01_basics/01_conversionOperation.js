f2f290d26884e56caeb306b3a88676b3d3257a02
akhildaz <akhilshiv65+github@gmail.com>
2025-11-15 08:01:16 +0000

    operations

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -27,13 +27,40 @@
 // "" => false
 // "akhil" => true
 
-let someNumber = 33;
-console.log(typeof someNumber);
-console.log(someNumber);
+// let someNumber = 33;
+// console.log(typeof someNumber);
+// console.log(someNumber);
 
 
-let stringSomeNumber = String(someNumber);
-console.log(typeof stringSomeNumber)
-console.log(stringSomeNumber);
+// let stringSomeNumber = String(someNumber);
+// console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber);
 
+
+// ******************************* Operations **************************
+
+// let value = 7;
+// let negatedValue = -value;
+// console.log(negatedValue);
+
+// console.log(2+2) add
+// 2-2 subtract
+// 2*2 multiply
+// 2/2 divide
+// 2 % 2 remainder
+// 2**2 power
+
+
+// let str1 = "hello"
+// let str2 = " akhil"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(+true); output: 1
+// console.log(+"") output: 0
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
 
