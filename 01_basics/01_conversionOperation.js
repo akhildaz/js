5f656ef394c1e27e9625ab413b3e126fe3aae1ab
akhildaz <akhilshiv65+github@gmail.com>
2025-11-15 07:28:53 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33abe";
+
+// let score = null; output: 0
+// let score = undefined; output: NaN
+// let score = true; output: 1
+// let score = false; output: 0
+// let score = "akhil" output: NaN
+
+// "33" => 33 but "33abe" => NaN
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "akhil" => true
+
+let someNumber = 33;
+console.log(typeof someNumber);
+console.log(someNumber);
+
+
+let stringSomeNumber = String(someNumber);
+console.log(typeof stringSomeNumber)
+console.log(stringSomeNumber);
+
+
