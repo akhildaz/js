553502f6e1a471263452aa15caf5a1cd734d3f0f
akhildaz <akhilshiv65+github@gmail.com>
2025-11-15 15:17:57 +0000

    strings part1

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,73 @@
+// const name = "akhil"
+
+// const repoCount = 50;
+
+// // console.log(name + repoCount + " Value");
+
+
+// // String interpolation
+
+// console.log(`Hello, my name is ${name}, and my repoCount is ${repoCount}!`);
+
+// const user = {
+//     userName: "Akhil",
+//     level: 100,
+// }
+
+// const now = new Date();
+
+// console.table(`
+// User: ${user.userName.toUpperCase()} 
+// Level: ${user.level} 
+// Time: ${now.toLocaleTimeString()} 
+// Status: ${user.level > 90 ? "Pro" : "Intermediate"}`);
+
+// output
+// User: AKHIL 
+// Level: 100 
+// Time: 2:49:55 PM 
+// Status: Pro
+
+// const userName = "Akhil"
+// console.log(userName[3]);
+// console.log(userAge);
+// console.log(typeof userAge);
+
+// console.log(userName.length);
+
+// let userAge = new Number(7);
+// userAge = 1
+
+
+// console.log(userAge);
+
+
+// mutation
+// let numObj = {age: 7}
+// let newNumObj = numObj
+// newNumObj.age = 10;
+// console.log(newNumObj);
+
+// let age = new Number(7);
+// let copy = age
+// console.log(age); output: [Number: 7]
+
+// age = 10;
+// console.log(copy); output: [Number: 7]
+
+// console.log(copy.valueOf()); output: 7
+
+// console.log(age); output: 10
+
+const name = "Akhil"
+
+// console.log(name.charAt(3));
+// console.log(name.indexOf('i'));
+
+// output
+// i
+// 3
+
+
+
+
