4a037bb0d3a75d1da9720c057f2417ccaf50d5b5
akhildaz <akhilshiv65+github@gmail.com>
2025-11-15 07:05:04 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,10 @@
+// "use strict";  treats all js code as newer version
+
+let name = "akhil"; // type string
+let age = 18; // type number
+let isLoggedIn = true; // type boolean
+
+let city = null // null -> standalone value
+let state; // this variable not defined during declaration is the value will be undefined until someone define it
+
+console.log(typeof state);
