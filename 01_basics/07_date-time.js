569137aba91cd54f13f931a515a6278102044c6f
akhildaz <akhilshiv65+github@gmail.com>
2025-11-16 09:12:55 +0000

    date time

---
+++ 01_basics/07_date-time.js
@@ -0,0 +1,25 @@
+// dates
+
+let now = new Date();
+console.log(now);
+console.log(now.toString());
+console.log(now.toLocaleString());
+console.log(now.toDateString());
+console.log(now.toISOString());
+console.log(now.toJSON());
+console.log(now.toTimeString());
+console.log(now.toLocaleDateString());
+console.log(now.toLocaleTimeString());
+console.log(now.toUTCString());
+
+// output
+// 11/16/2025, 9:01:40 AM
+// Sun Nov 16 2025
+// 2025-11-16T09:01:40.209Z
+// 2025-11-16T09:01:40.209Z
+// 09:01:40 GMT+0000 (Coordinated Universal Time)
+// 11/16/2025
+// 9:01:40 AM
+// Sun, 16 Nov 2025 09:01:40 GMT
+
+// console.log(typeof now); output: object
